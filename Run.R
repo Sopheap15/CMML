ce6087328a90b3cb9f78de39df200ec259ac26dd
Sopheap <sopheap15@gmail.com>
2022-07-28 09:02:41 +0700

    testing

--- Run.R
+++ Run.R
@@ -3,6 +3,6 @@ source("code/Load_data.R",local = knitr::knit_global())
 
 # Render report
 rmarkdown::render(
-  input = "code/CMML_report.Rmd",
+  input = "code/CMML_report.Rmd", encoding = 'UTF-8',
   output_file = paste0("../Output/CMML Report ",Sys.Date(),".html"))
 
