7e03621396fd4bf6fc9d9c1f5950b8136e768468
Sopheap <sopheap15@gmail.com>
2022-07-28 16:52:30 +0700

    Run.R

--- Run.R
+++
@@ -1,8 +0,0 @@
-# Load data ( select all => crt/cmd + enter) ----
-source("code/Load_data.R",local = knitr::knit_global())
-
-# Render report
-rmarkdown::render(
-  input = "code/CMML_report.Rmd", encoding = 'UTF-8',
-  output_file = paste0("../Output/CMML Report ",Sys.Date(),".html"))
-#Hello
