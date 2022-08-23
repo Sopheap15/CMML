13397535a6288a6b22ee41768c8294ff90d824a5
Sopheap <sopheap15@gmail.com>
2022-08-23 13:39:02 +0700

    adding style

--- Run.R
+++ Run.R
@@ -1,3 +1,4 @@
+# version 2 reading files from data with ";"
 # Load data ( select all => crt/cmd + enter) ----
 source("code/Load_data.R",local = knitr::knit_global())
 
@@ -5,4 +6,3 @@ source("code/Load_data.R",local = knitr::knit_global())
 rmarkdown::render(
   input = "code/CMML_report.Rmd", encoding = 'UTF-8',
   output_file = paste0("../output/CMML Report ",Sys.Date(),".html"))
-#Hello
