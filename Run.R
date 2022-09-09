612601e930640cfa39ca20f3b7a80015e04b29f6
Sopheap <sopheap15@gmail.com>
2022-09-09 11:33:38 +0700

    move map data

--- Run.R
+++ Run.R
@@ -1,8 +1,12 @@
 # version 2 reading files from data with ";"
 # Load data ( select all => crt/cmd + enter) ----
-source("code/Load_data.R",local = knitr::knit_global())
+source("code/Load_data.R", 
+			 local = knitr::knit_global())
 
 # Render report
-rmarkdown::render(
-  input = "code/CMML_report.Rmd", encoding = 'UTF-8',
-  output_file = paste0("../output/CMML Report ",Sys.Date(),".html"))
+rmarkdown::render(input = "code/CMML_report.Rmd", 
+									encoding = "UTF-8",
+									output_format = "html_document",
+									output_dir = "output",
+									output_file = paste0("CMML Report ", Sys.Date()),
+									quiet = T)
--- code/map.R
+++ code/map.R
@@ -1,18 +1,23 @@
 # map data-----
 # read boundary map
-map <- list.files("khm_adm1_un", pattern = "khm_adm1_un.shp", full.names = T) %>% 
+map <- list.files("code/khm_adm1_un",
+									pattern = "khm_adm1_un.shp",
+									full.names = T) %>%
 	read_sf()
+
 map <- sf::st_make_valid(map)
 
 # data
-m_data <- data %>% 
-	filter(!is.na(customer), delivery_date >= date[3, 2], delivery_date < date[4, 2]) %>% 
-	mutate(customer = recode(customer,
-													 "BTB_AFRIMS" = "Battambang",
-													 "National Public Health Laboratory - AMR" = "NPHL",
-													 "National Public Health Laboratory - EGASP" = "NPHL",
-													 "NHealth Laboratory" = "Royal Phnom Penh hospital" )) %>%
+m_data <- data %>%
+	filter(!is.na(customer), delivery_date >= date[3, 2], delivery_date < date[4, 2]) %>%
+	mutate(ustomer = recode(customer,
+													"BTB_AFRIMS" = "Battambang",
+													"National Public Health Laboratory - AMR" = "NPHL",
+													"National Public Health Laboratory - EGASP" = "NPHL",
+													"NHealth Laboratory" = "Royal Phnom Penh hospital"
+	)
+	) %>% 
 	group_by(customer, x, y) %>%
-	summarise(Total = sum(d_quantity, na.rm = T)) %>% 
-	drop_na() %>% 
-	st_as_sf(coords = c("x","y"), crs = 4326)
+	summarise(Total = sum(d_quantity, na.rm = T)) %>%
+	drop_na() %>%
+	st_as_sf(coords = c("x", "y"), crs = 4326)
