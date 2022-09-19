12a6a82d5c5eaf2ffc8be36f75f74729cba07bca
Sopheap <sopheap15@gmail.com>
2022-09-19 09:16:02 +0700

    update map

--- code/map.R
+++ code/map.R
@@ -3,7 +3,10 @@
 map <- list.files("code/khm_adm1_un",
 									pattern = "khm_adm1_un.shp",
 									full.names = T) %>%
-	read_sf()
+	read_sf() %>% 
+	select(-any_of(c("disp_area", "validity", "last_modif"))) %>% 
+	mutate(adm1_altnm = str_remove(adm1_altnm, ".*(?=,).."),
+				 adm1_altnm = recode(adm1_altnm, 'Tonlé Sap Lake' = "ទន្លេសាប")) 
 
 map <- sf::st_make_valid(map)
 
@@ -15,9 +18,9 @@ m_data <- data %>%
 													"National Public Health Laboratory - AMR" = "NPHL",
 													"National Public Health Laboratory - EGASP" = "NPHL",
 													"NHealth Laboratory" = "Royal Phnom Penh hospital"
-	)
+	                       )
 	) %>% 
 	group_by(customer, x, y) %>%
-	summarise(Total = sum(d_quantity, na.rm = T)) %>%
+	summarise(Purchased = paste(format(sum(d_quantity, na.rm = T), big.mark = ","), "units")) %>%
 	drop_na() %>%
 	st_as_sf(coords = c("x", "y"), crs = 4326)
